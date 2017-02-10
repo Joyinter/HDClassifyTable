c667b74a9eed59ce88e655465f0748d7dc328713
Joyinter <hljjxmhd@126.com>
2017-02-10 09:46:46 +0800

    Initial Commit

---
+++ HDClassifyTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HDClassifyTable
+//
+//  Created by Delano on 2017/2/10.
+//  Copyright © 2017年 delano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
